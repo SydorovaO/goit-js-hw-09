1edf8f56331887efeec2d04667cc8ce5d99d1910
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-22 13:56:08 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+console.log(startBtn);
+console.log(stopBtn);
+stopBtn.setAttribute('disabled', 'true');
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    console.log('ggggggggggggg');
+    document.body.style.backgroundColor = getRandomHexColor();
+
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', 'true');
+  }, 1000);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', 'true');
+  startBtn.removeAttribute('disabled');
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
