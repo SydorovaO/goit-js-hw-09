12303647d2558d6c9fad45cc9b1d0245dbc71bc3
SydorovaO <112278466+SydorovaO@users.noreply.github.com>
2023-05-23 16:38:21 +0000

    Deploying to gh-pages from @ SydorovaO/goit-js-hw-09@bf4bf23f8cb7523eb74b13477e34f6ec4513e08b 🚀

---
+++ 02-timer.35b9ad08.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName("timer"),document.getElementsByClassName("field"),document.getElementsByClassName("value"),document.getElementsByClassName("label"),document.querySelector("span[data-days]"),document.querySelector("span[data-hours]"),document.querySelector("span[data-minutes]"),document.querySelector("span[data-seconds]");
+//# sourceMappingURL=02-timer.35b9ad08.js.map
