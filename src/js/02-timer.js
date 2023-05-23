5eacc66ad6846c266f0a8333d1a776d897d00da6
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-23 19:37:17 +0200

    add flatpickr to 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,8 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 const refs = {
+  //   datetimePickerEl: document.getElementById('datetime-picker'),
   timer: document.getElementsByClassName('timer'),
   field: document.getElementsByClassName('field'),
   value: document.getElementsByClassName('value'),
@@ -8,4 +12,41 @@ const refs = {
   minutes: document.querySelector('span[data-minutes]'),
   seconds: document.querySelector('span[data-seconds]'),
 };
-const { timer, hours, minutes, seconds } = refs;
+const { datetimePickerEl, timer, hours, minutes, seconds } = refs;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    console.log(selectedDate);
+    setInterval(() => {
+      const currentDate = new Date();
+
+      console.log(convertMs(selectedDate - currentDate));
+    }, 5000);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
